2d62b78511637edd80a79eee32e376c617e11e5b
bornartem <born.artem@gmail.com>
2023-08-25 16:10:36 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void printNumbers() {
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < 10; i++) {
+            System.out.print(numbers[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0) {
+                System.out.print(numbers[i] + 1);
+            }
+        }
+        System.out.println();
+        for (int i = 2; i < numbers.length - 3; i++) {
+            System.out.print(numbers[i]);
+        }
+    }
 
+    public static void main(String[] args) {
+        printNumbers();
     }
 }
